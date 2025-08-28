7333d70c907cf65bb221d35da5b20d1c5c969739
Leigh Pauls <leighpauls@Leighs-MacBook-Pro.local>
2025-08-28 16:36:43 -0700

    fix bindgen config and initialization

--- bindgen.sh
+++ bindgen.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-wasm-bindgen --out-dir . --out-name game --target web ~/src/manytris/target/wasm32-unknown-unknown/debug/manytris.wasm
+wasm-bindgen \
+    --out-dir . \
+    --out-name game \
+    --target web \
+    --no-typescript \
+    ~/src/manytris/target/wasm32-unknown-unknown/debug/manytris.wasm
